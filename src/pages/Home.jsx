0d4bba05cf1c15130579fee71b60f7b5b9388c68
hansolChoi29 <ggksthf29@gmail.com>
2024-12-06 01:40:31 +0900

    주석 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,15 @@ export default function Home() {
     // 쿼리키는 반드시 배열이다.
     queryFn: async () => {
       // 쿼리펑션은 서버에서 데이터 불러오고 상태변경 요청 두가지 동작 함.
+      // isPending은 QueryClient에 데이터가 아직 없고
+      // 쿼리가 실행 대기중일 때. 즉, queryFn실행 전.
+      // 쿼리키는 있는데 아래가 null이면 isPending이 true가 됨.
       const response = await todoApi.get("/todos?_sort=-createdAt");
       return response.data;
     },
   });
-
+  // isFetching= queryFn이 실행 될 때마다,
+  // 클라이언트가 서버에 요청하고나서 응답 기다리는 중
   if (isPending) {
     return <div style={{ fontSize: 36 }}>로딩중...</div>;
   }
